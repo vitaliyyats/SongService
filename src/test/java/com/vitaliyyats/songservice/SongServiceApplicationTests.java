326d402f9dd5f7f4e6843a0695c2da24b145c78a
Vitalii_Yatsunskyi <vitalii_yatsunskyi@epam.com>
2023-12-22 14:18:34 +0200

    Initial commit

---
+++ src/test/java/com/vitaliyyats/songservice/SongServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitaliyyats.songservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
