059e965efee989abe67d7069532cd3fd5357debd
Vitalii_Yatsunskyi <vitalii_yatsunskyi@epam.com>
2023-12-28 12:20:49 +0200

    added test with test container

--- src/main/java/com/vitaliyyats/songservice/dto/SongCreationResponse.java
+++ src/main/java/com/vitaliyyats/songservice/dto/SongCreationResponse.java
@@ -2,8 +2,10 @@ package com.vitaliyyats.songservice.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class SongCreationResponse {
     private Long id;
---
+++ src/test/java/com/vitaliyyats/songservice/controller/SongsControllerTest.java
@@ -0,0 +1,54 @@
+package com.vitaliyyats.songservice.controller;
+
+import com.vitaliyyats.songservice.dto.SongCreationResponse;
+import com.vitaliyyats.songservice.dto.SongDTO;
+import com.vitaliyyats.songservice.repository.SongRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.http.ResponseEntity;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.util.Objects;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Testcontainers
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class SongsControllerTest {
+
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:latest");
+
+    @Autowired
+    SongRepository songRepository;
+
+    @Autowired
+    TestRestTemplate restTemplate;
+
+    @Test
+    void shouldCreateSong() {
+        ResponseEntity<SongCreationResponse> response = restTemplate.postForEntity("/songs", songDTO(), SongCreationResponse.class);
+        var savedSong = songRepository.findById(Objects.requireNonNull(response.getBody()).getId());
+        assertThat(savedSong.isPresent()).isEqualTo(true);
+    }
+
+
+    private SongDTO songDTO() {
+        return SongDTO.builder()
+                .name("We are the champions")
+                .artist("Queen")
+                .album("News of the world")
+                .length("2:59")
+                .year(1977)
+                .resourceId("1")
+                .build();
+    }
+
+
+}
